28983e515fe4cc903768059077d1d6183814083e
Mahmoud Kassem <Mahmoud Kassem@DESKTOP-FSH1TEE>
2022-06-06 13:19:34 +0200

    Add project files.

---
+++ RepositoryPatternWithUnitOfWork.API/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using RepositoryPatternWithUnitOfWork.EF.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
